1928742e1158196cb2ed2c8a05cbbb21d8ded5e1
Esteban12f <joehv33@gmail.com>
2024-12-01 22:05:22 -0500

    Terminado 2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        ClaseGenerica <String> objString = new ClaseGenerica<String>("Hola");
+        ClaseGenerica <String> objString = new ClaseGenerica<String>("Hola"); 
+        // Se crea un objeto especificando el tipo de dato que va a tener
         objString.tipoClase();
+        // Se llama al metodo para imprimir el tipo de dato al que corresponde
 
-        ClaseGenerica <Integer> objInt = new ClaseGenerica<Integer>(40);
-        objInt.tipoClase();
+        ClaseGenerica <Integer> objInteger = new ClaseGenerica<Integer>(40);
+        objInteger.tipoClase();
     }
 }
--- src/ClaseGenerica.java
+++ src/ClaseGenerica.java
@@ -1,11 +1,18 @@
 public class ClaseGenerica <T> {
-    T atributo;
+    /*
+     * El termino "<T>" indica y especifica que la clase sera generica lo cual nos permite 
+     * crear una variable con un valor generico (double, String, Integer)
+     */
 
-    public ClaseGenerica(T atributo2) {
-        this.atributo = atributo2;
+    T atributo; // Se crea la variable q tendra un valor generico
+
+    public ClaseGenerica(T atributo2) { // Se crea un constructor que recibira un parametro generico
+        this.atributo = atributo2; 
+        /*El atributo inicialmente creado tomara el 
+        valor del parametro q recibe el constructor */ 
     }
 
-    public void tipoClase(){
+    public void tipoClase(){ //Metodo para imprimir el tipo de la clase
         System.out.println("El tipo de dato T es: " + atributo.getClass());
     }
     
